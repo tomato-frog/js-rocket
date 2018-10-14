43293a4382199a4436a4dc0568f0b99d0cc127bd
Sofya Kholodkova <reima.cx@gmail.com>
2018-10-14 19:55:50 +0700

    add style

--- js/rocket.js
+++ js/rocket.js
@@ -22,7 +22,7 @@ var changeState = function (state) {
                 console.log('randomNumber:'+randomNumber)
 
                 //succes
-                if (randomNumber > 5) {
+                if (randomNumber > 3) {
                     changeState(4);//succes
                 } else {
                     changeState(5);//oh no!
